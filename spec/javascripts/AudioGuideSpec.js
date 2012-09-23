9a953d3ad56ba52aa370a9db7721f20ee8ace8e5
Anthony Green <anthony.green@bbc.co.uk>
2012-09-23 10:35:03 +0100

    Add 2nd spec for AudioGuide

--- spec/javascripts/AudioGuideSpec.js
+++ spec/javascripts/AudioGuideSpec.js
@@ -5,25 +5,45 @@ describe("AudioGuide", function() {
   
   var firstLocation = new Location("First location");
   var nextLocation =  new Location("Next location");
+  var lastLocation =  new Location("Last location");
   var firstTrack = new Track("Track for first location");
   var nextTrack = new Track("Track for next location");
   var lastTrack = new Track("Track for last location");
+
+  stub = sinon.stub(mediaLibrary, 'trackFor');
+  stub.withArgs(firstLocation).returns(firstTrack);
+  stub.withArgs(nextLocation).returns(nextTrack);
+  stub.withArgs(lastLocation).returns(lastTrack);
+  
   var guide;
   
   
   beforeEach(function() {
-    sinon.stub(mediaLibrary, 'trackFor').withArgs(firstLocation).returns(firstTrack).withArgs(nextLocation).returns(nextTrack);
     guide = new AudioGuide(mediaLibrary, mediaPlayer.object);
   });
 
   it("plays media track for initial location", function() {
     
-    mediaPlayer.expects("play").withArgs(firstTrack).once();
+    mediaPlayer.expects("play").withArgs(firstTrack);
     
     guide.locationChanged(firstLocation);
     
     mediaPlayer.verify();
-  
+      
   });
   
+  it("waits for current track to finish before playing track for new location", function() {
+    
+    mediaPlayer.expects("play").withArgs(firstTrack);
+    mediaPlayer.expects("play").withArgs(nextTrack);
+    
+    guide.locationChanged(firstLocation);
+      
+    guide.locationChanged(nextLocation);
+    guide.trackFinished();
+      
+     mediaPlayer.verify();
+   
+  });
+    
 });
