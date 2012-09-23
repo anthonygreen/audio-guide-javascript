98eb3118be465d1e4e8371cc9f6e92fa7e021f39
Anthony Green <anthony.green@bbc.co.uk>
2012-09-23 20:23:22 +0100

    Update Location Tracker specs

--- spec/javascripts/LocationTrackerSpec.js
+++ spec/javascripts/LocationTrackerSpec.js
@@ -9,14 +9,11 @@ describe("LocationTracker", function() {
     var listener = sinon.mock({ locationChanged: function() {} }); 
     var reverseGeocoder = new ReverseGeocoder;
     
-     
     sinon.stub(reverseGeocoder, 'locationOf').withArgs(position).returns(location);
          
     listener.expects('locationChanged').withArgs(location);
     
-    
     var tracker = new LocationTracker(reverseGeocoder, listener.object);
-    
       
     tracker.positionChanged(position);
     
@@ -24,6 +21,52 @@ describe("LocationTracker", function() {
   });
   
   
+  it("reports when location changes", function() {
+		var position1 = new Position(1,2);
+		var position2 = new Position(3,4);
+		var location1 = new Location("South Kensington");
+		var location2 = new Location("Gloucester Road");
+    var listener = sinon.mock({ locationChanged: function() {} }); 
+    
+    var reverseGeocoder = new ReverseGeocoder;
+    
+    var stub = sinon.stub(reverseGeocoder, 'locationOf');
+    stub.withArgs(position1).returns(location1);
+    stub.withArgs(position2).returns(location2);
+    
+    listener.expects('locationChanged').withArgs(location1).once();
+    listener.expects('locationChanged').withArgs(location2).once();
+    
+    var tracker = new LocationTracker(reverseGeocoder, listener.object);
+    
+    tracker.positionChanged(position1);
+    tracker.positionChanged(position2);
+    
+    listener.verify();
+    
+  });
   
+  it("does not report when location stays the same", function() {
+		var position1 = new Position(1,2);
+		var position2 = new Position(3,4);
+		var location = new Location("South Kensington");
+    var listener = sinon.mock({ locationChanged: function() {} }); 
+
+    var reverseGeocoder = new ReverseGeocoder;
+    
+    var stub = sinon.stub(reverseGeocoder, 'locationOf');
+    stub.withArgs(position1).returns(location);
+    stub.withArgs(position2).returns(location);
+    
+    listener.expects('locationChanged').withArgs(location).once();
+    
+    var tracker = new LocationTracker(reverseGeocoder, listener.object);
+       
+		tracker.positionChanged(position1);
+		tracker.positionChanged(position2);    
+    
+    listener.verify();
+    
+  });
   
 });
